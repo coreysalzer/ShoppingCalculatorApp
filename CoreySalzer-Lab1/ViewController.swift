2c1ad2fb4ad4568b0e7bc51b623578021d10f50b
Corey Salzer <coreysalzer@wustl.edu>
2017-01-31 00:23:38 -0600

    Initial Commit

---
+++ CoreySalzer-Lab1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreySalzer-Lab1
+//
+//  Created by Corey Salzer on 1/31/17.
+//  Copyright © 2017 Corey Salzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
